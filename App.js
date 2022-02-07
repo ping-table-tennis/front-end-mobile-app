e2d5a0f9ae7a2efb9faea2ad0b9926f714ff57cd
Yasiris Ortiz <yasirisortiz24@gmail.com>
2022-02-07 10:27:26 -0500

    small changes

--- App.js
+++ App.js
@@ -17,7 +17,7 @@ export default function App() {
       <Stack.Navigator>
 	  	{/* <Stack.Screen name="Start" options={{headerShown: false}} component={Started} /> */}
         <Stack.Screen name="Login" options={{headerShown: false}} component={LoginScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
+        {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
       </Stack.Navigator>
     </NavigationContainer>
   );
