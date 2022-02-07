a118dd2a282a6b3788349593fe4411595d2d40ef
Yasiris Ortiz <yasirisortiz24@gmail.com>
2022-02-07 10:22:36 -0500

    merging

--- App.js
+++ App.js
@@ -9,25 +9,6 @@ import HomeScreen from './screens/HomeScreen';
 import Started from "./Views/Started";
 
 
-
-/*
-import * as firebase from 'firebase';
-
-// firebase setup
-const firebaseConfig = {
-  apiKey: "AIzaSyDtW3e_WVrh80YjG87_-w6T2GJJyhpTC-Y",
-  authDomain: "pingtt-584c4.firebaseapp.com",
-  projectId: "pingtt-584c4",
-  storageBucket: "pingtt-584c4.appspot.com",
-  messagingSenderId: "408950196090",
-  appId: "1:408950196090:web:e05ec7d9fc49af0c8cf8c3",
-  measurementId: "G-SRLPWK25W5"
-};
-
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-*/
 const Stack = createNativeStackNavigator();
 
 export default function App() {
