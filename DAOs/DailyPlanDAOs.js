f15d161fdeb7669979deeea9f0a99e6e55ff0225
vperls000 <53792766+vperls000@users.noreply.github.com>
2022-03-22 17:27:21 -0400

    updates to daily plan DAO

--- DAOs/DailyPlanDAOs.js
+++ DAOs/DailyPlanDAOs.js
@@ -12,12 +12,6 @@ export class DailyPlan {
 
         console.log(this.coachEmail);
     }
-
-    setDate = async (date) => {
-        this.date = date;
-        const res = await db.collection('Daily Plans').doc(this.id).update({date: this.date});
-
-    }
 }
 
 // This function puts the new daily plan in the database
@@ -34,6 +28,23 @@ export const init = async (plan) => {
     console.log(res.id);
 }
 
+// Regardless of what has been updated in the DAO, the entire object in firestore will be updated
+export const updateDailyPlan = async (plan) => {
+    const data = {
+        checklist_tasks: plan.checklistTasks,
+        checklist_iscompleted: plan.checklistIsCompelted,
+        emails: [plan.coachEmail, plan.studentEmail],
+        date: firebase.firestore.Timestamp.fromDate(plan.date)
+    }
+
+    const res = await db.collection('Daily Plans').doc(plan.id).update(data);
+}
+
+export const deleteDailyPlan = async (plan) => {
+    await db.collection('Daily Plans').doc(plan.id).delete();
+}
+
+// gets the daily plans for either the the coach email or the student email.
 export const getDailyPlans = async (email) => {
     const dailyPlansRef = db.collection('Daily Plans');
     const snapshot = await dailyPlansRef.where('emails', 'array-contains', email).get();
--- DAOs/GeneralPlanDAOs.js
+++ DAOs/GeneralPlanDAOs.js
@@ -25,3 +25,24 @@ export const init = async (plan) => {
         console.log('successfully added to database')
     }
 }
+
+export const deleteDailyPlan = async (plan) => {
+    await db.collection('Daily Plans').doc(plan.id).delete();
+}
+
+// gets the general plan for either the the coach email or the student email.
+export const getGeneralPlan = async (email) => {
+    const snapshot = await db.collection('General Plans').where('emails', 'array-contains', email).get();
+    const userDailyPlans = [];
+
+    if(snapshot.empty){
+        console.log('No Daily Plans for ', email);
+    }
+    else {
+        snapshot.forEach(doc => {
+            userDailyPlans.push(doc.data());
+        });
+    }
+
+    return userDailyPlans;
+}
