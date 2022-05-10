d84f47eb9f52f2484ad5430c77681374394f4275
Yasiris Ortiz <yasirisortiz24@gmail.com>
2022-05-10 08:39:07 -0400

    h

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -215,3 +215,62 @@ const styles = StyleSheet.create({
         marginTop: 5,
     },
 })
+class Card extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        editing: false
+      };
+      this.newbirthYear = "";
+      this.newHomeWorld = "";
+    }
+  
+    render() {
+      const { imgSrc, cardName, birthYear, homeWorld, onEdit } = this.props;
+      return (
+        <div className="card">
+          <div className="card-content">
+            <div className="card-name">{cardName}</div>
+            <img src={`http://localhost:3008/${imgSrc}`} alt="profile" />
+            <p>
+              <span>Birthday:</span>
+              {this.state.editing ? (
+                <span className="birth-year">{birthYear}</span>
+              ) : (
+                <input
+                  type="text"
+                  defaultValue={birthYear}
+                  ref={node => {
+                    this.newbirthYear = node;
+                  }}
+                />
+              )}
+            </p>
+            <p>
+              <span>Homeworld:</span>
+              {this.state.editing ? (
+                <span className="home-world">{homeWorld}</span>
+              ) : (
+                <input
+                  type="text"
+                  defaultValue={homeWorld}
+                  ref={node => {
+                    this.newHomeWorld = node;
+                  }}
+                />
+              )}
+            </p>
+            <div align="center">
+              <button
+                onClick={() => {
+                  this.setState({ editing: true });
+                }}
+              >
+                Edit
+              </button>
+            </div>
+          </div>
+        </div>
+      );
+    }
+  }
